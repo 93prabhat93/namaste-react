8af5e8e8854d6d11bf76139380674d5d63f2b5dd
93prabhat93 <prabhat.panda57@gmail.com>
2023-07-01 23:30:33 +0530

    Update App.js
    inception

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
-const heading = React.createElement(
-    'h1',
-    {
-        id:'heading'
-    },
-    'Welcome from React');
+// const heading = React.createElement(
+//     'h1',
+//     {
+//         id:'heading'
+//     },
+//     'Welcome from React');
+/* <div id="header">
+    <div id='child'>
+        <h1 id="header">
+
+        </h1>
+    <div/>
+</div> */
+const parent = React.createElement(
+    'div',
+    {id:'parent'},
+    [
+        React.createElement(
+            'div',
+            {id:'child1'},
+            React.createElement('h1',{id:'header'},'react header 1')
+        ),
+        // React.createElement(
+        //     'div',
+        //     {id:'child2'},
+        //     React.createElement('h1',{id:'header'},'react header 2')
+        // )
+    ])
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading)
+root.render(parent)
