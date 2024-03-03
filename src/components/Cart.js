c96f1b957719c8f93e878c1017b2a9ef04c1046c
93prabhat93 <prabhat.panda57@gmail.com>
2024-03-03 21:31:08 +0530

    another commit

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,11 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
+import { useState } from "react";
 import {IMG_CDN_URL} from '../utils/constants'
 import { addItem, clearCart } from "../utils/cartSlice"
 import { useDispatch } from "react-redux";
 import { removeItem } from "../utils/cartSlice";
 
 const Cart = () =>{
-
     const cartItems = useSelector((store) => store.cart.items)
     const dispatch = useDispatch()
     const handleClearCart = () =>{
@@ -17,6 +17,7 @@ const Cart = () =>{
     const addItemToCart = (items) =>{
         dispatch(addItem(items))
     }
+
     return (
         <div className="cart-page">
             <div className="action-box">
@@ -25,6 +26,7 @@ const Cart = () =>{
             </div>
             
             {
+                cartItems.length === 0 ? <h3>Add items to the cart </h3>:
                  cartItems.map((items,index) => (
                            
                     <div  key={items.card.info.id+index} className="menu-item-box">
@@ -33,7 +35,9 @@ const Cart = () =>{
                             <p>{items.card.info.description} - <span>{items.card.info.itemAttribute.vegClassifier}</span></p>
                         </div>
                         <div className="width-30-percent">
+                            <div className="image-box">
                            { items.card.info.imageId ? <img className="product-img" src={IMG_CDN_URL +items.card.info.imageId}></img>:<p className="not-available-text">N/A</p>}
+                           </div>
                            <div className="action-box margin-zero">
                                 <button onClick={()=>addItemToCart(items)} className="cart-add-remove">Add</button>
                                 <button onClick={()=>removeItemFromCart(index)} className="cart-add-remove">Remove</button>
